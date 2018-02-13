1d4c5681a2b14fe5a61ecc87555af3a9e6505976
Raz1789 <navaneeth_3@hotmail.com>
2018-02-13 13:19:59 -0500

    Starting with MapNode

---
+++ Source/Geodroid/MapNode.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MapNode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GEODROID_API UMapNode : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
